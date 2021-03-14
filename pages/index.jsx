9af8a419ae4adf3ed12cf5879fe3bfd9ce971f76
vitrh <vinhthong.trinh@hotmail.com>
2021-03-14 16:50:45 +0100

    first init

---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap"
+          rel="stylesheet"
+        ></link>
+        <title>Vinh's Portfolio</title>
+      </Head>
+
+    </div>
+  )
+}
